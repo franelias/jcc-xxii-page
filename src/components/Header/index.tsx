c47ce25ea1278dad5b9060594e4003a99b45fff8
Francisco Elías <fielias81@gmail.com>
2024-05-12 11:52:51 -0300

    wip

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,40 +1,50 @@
-import React, { PropsWithChildren } from "react";
+import React from "react";
+import { Layout } from "antd";
+import {
+    BiLogoInstagram,
+    BiLogoYoutube,
+    BiMailSend,
+    BiSolidMap,
+    BiSolidPhone,
+} from "react-icons/bi";
 
 import "./styles.scss"
-import classNames from "classnames";
-import NavItem from "../NavItem";
-import Title from "../Title";
+import Link from "../Link";
 
-const Header: React.FC<PropsWithChildren> = ({ children }: PropsWithChildren) => {
+const Header: React.FC = () => {
+    const { Header } = Layout;
+
+    const items = new Array(3).fill(null).map((_, index) => ({
+        key: String(index + 1),
+        label: `nav ${index + 1}`,
+    }));
     return (
-        <header
-            className={classNames(
-                "fixed w-full top-0 flex min-h-[40px] ease-in-out items-center justify-between bg-[--secondary-color] text-white "
-                // "fixed w-full top-0 z-30 flex flex-wrap items-center justify-between text-gray-800 bg-black min-h-[2px] md:justify-start md:text-xl md:gap-7 px-7 py-5 duration-300 ease-in-out",
-            )}
+        <Header
+            className="bg-[--secondary-color] justify-between"
+            style={{
+                position: 'sticky',
+                top: 0,
+                zIndex: 1,
+                width: '100%',
+                display: 'flex',
+                alignItems: 'center',
+            }}
         >
-            <Title>
-                <NavItem >JCC 2023</NavItem>
-            </Title>
-            {/* <Nav>
-                <NavItem scroll={() => scrollTo(infoRef)}>Info</NavItem>
-                <NavItem scroll={() => scrollTo(cronogramaRef)}>Cronograma</NavItem>
-                <NavItem scroll={() => scrollTo(actividadesRef)}>
-                    Actividades
-                </NavItem>
-                <NavItem scroll={() => scrollTo(apoyoRef)}>Apoyo</NavItem>
-                <NavItem scroll={() => scrollTo(jobSearchRef)}>Búsqueda</NavItem>
-            </Nav>
-            <MobileNav showHeader={showHeader}>
-                <NavItem scroll={() => scrollTo(infoRef)}>Info</NavItem>
-                <NavItem scroll={() => scrollTo(cronogramaRef)}>Cronograma</NavItem>
-                <NavItem scroll={() => scrollTo(actividadesRef)}>
-                    Actividades
-                </NavItem>
-                <NavItem scroll={() => scrollTo(apoyoRef)}>Apoyo</NavItem>
-                <NavItem scroll={() => scrollTo(jobSearchRef)}>Búsqueda</NavItem>
-            </MobileNav> */}
-        </header >
+            <div className="mr-4">JCC 2024</div>
+            <div className="flex c">
+                <span className="transition-colors hover:text-pink-700 mr-4">
+                    <Link url="https://www.instagram.com/jccfceia">
+                        <BiLogoInstagram size={24}/>
+                    </Link>
+                </span>
+                <span className="transition-colors hover:text-pink-700">
+                    <Link url="https://www.youtube.com/channel/UC-CReVEx4-3AfJOH1Tr-udw">
+                        <BiLogoYoutube size={24}/>
+                    </Link>
+                </span>
+            </div>
+
+        </Header>
     );
 };
 
